7873ae333828d39c8185757589464b0b4535127f
dr.N0 <l0psided0ctor@gmail.com>
2026-05-22 00:38:57 -0400

    watchdog 3.4
    this update formalizes the watchdog file system packing the sourcemod metamod plugin within. this software is in 100% intended functionality, 100% intended stability, and works for 95% of all source engine dedicated servers.
    this is full official release. the core platforming moving forward now.
    watchdogs official website is found here
    http://drn0.site.nfoservers.com/hub/drn0/to0lb0x/watchdog/index.php
    and the sourcemod metamod plugins support from the federation master list
    http://drn0.site.nfoservers.com/hub/drn0/to0lb0x/watchdog/about.php

--- watchdog/config/servers.ps1
+++ watchdog/config/servers.ps1
@@ -1,5 +1,11 @@
-# --- Server Definitions ---
-# Set RconPassword for each server to enable player monitoring and custom RCON commands.
+# -----------------------------------------------------------------------
+# --- Source Dedicated WATCHDOG by dr.N0 --------------------------------
+# --- http://drn0.site.nfoservers.com/hub/drn0/to0lb0x/watchdog/index.php
+# --- Visit the WATCHDOG MASTER LIST at the official WATCHDOG website.
+# -----------------------------------------------------------------------
+
+# --- Server Definitions File ---
+# Set RconPassword for each server to enable player monitoring and RCON commands.
 #
 # Set Region by this strict list of names
 # ---
@@ -17,12 +23,18 @@ $masterServerEnabled = $true
 # --- Name your watchdog Node --- 
 $watchdogNodeName = "My WATCHDOG Node"
 
+# --- do not touch root directories ---
 $masterHeartbeatFile = Join-Path $scriptRoot "data\master_heartbeat.json"
-
+# --- do not touch master server url ---
 $masterServerUrl = "http://drn0.site.nfoservers.com/hub/drn0/to0lb0x/watchdog/masterlist.php"
-
+# --- do not touch master api key ---
 $masterApiKey = "SDW_26_V2_NML_H01OBWOME301DW5D10D99ANH60660"
 
+# --- Start server definition between @{ and }, | only ending code blocks
+# --- end in a single closing bracket }
+# --- path exe and exe must match! port and rcon port must match!
+# --- must contain actual servers rcon password set in server.cfg!
+# --- Args should include -console -game +map -ip -port +exec server.cfg
 $servers = @(
     @{
         Name = "sample1"
